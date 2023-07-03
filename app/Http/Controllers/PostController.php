2f0a0835466ee04b6192f1eb4e0929a4c6870967
Abimahub <indigosaurus@hotmail.co.uk>
2023-07-03 14:46:20 +0000

    .

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Validation\Rule;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(
+                        request(['search', 'category', 'author'])
+                    )->paginate(18)->withQueryString()
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+}
